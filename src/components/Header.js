940ef6a98ff0c9bcd5da6786a1528a9a3c52302a
Bilal Khan <bilalwalker687@gmail.com>
2024-05-04 18:03:47 +0500

    added login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,8 +31,8 @@ const Header = (props) => {
                     <img src="/images/series-icon.svg" alt="Home"></img>
                     <span>SERIES</span>
                 </a>
-            
             </NavMenu>
+            <Login>Login</Login>
         </Nav>
     )
 };
@@ -126,9 +126,25 @@ const NavMenu = styled.div`
         }
     }
 
-    // @media(max-width: 768px) {
-    //     display: none;
-    // }
+    @media(max-width: 768px) {
+        display: none;
+    }
+`;
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all .2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
 `;
 
 export default Header;
