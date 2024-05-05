43188622e5ba7e443fcb1729363d935c7474954d
Bilal Khan <bilalwalker687@gmail.com>
2024-05-05 17:15:01 +0500

    added data to firestore database added Movies in the Recommends section (not displaying correctly)

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, configures, getDefaultMiddleware } from  "@reduxjs/toolkit"
 import userReducer from "../features/user/userSlice"
+import movieReducer from "../features/movie/movieSlice"
 
 // setting up the redux store
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,52 @@ import Recommends from "./Recommends";
 import NewDisney from "./NewDisney";
 import Originals from "./Originals";
 import Trending from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisney = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                console.log(recommends);
+                switch(doc.data().type) {
+                    case "recommend":
+                        recommends = [...recommends, { id: doc.id, ...doc.data() } ]
+                        break;
+                    case 'new':
+                        newDisney = [...newDisney, { id: doc.id, ...doc.data() }]
+                        break;
+                    case 'original':
+                        originals = [...originals, { id: doc.id, ...doc.data() }]
+                        break;
+                    case 'trending':
+                        trending = [...trending, { id: doc.id, ...doc.data() }]
+                        break;
+                }
+            });
+        
+
+            dispatch(
+                setMovies({
+                    recommend: recommends,
+                    newDisney: newDisney,
+                    original: originals,
+                    trending: trending,
+                })
+            )
+        });
+    }, [userName]); // only want it to be updated when userName is updated.when user logs in, grab this data from firestore and store it in redux store
+
     return(
         <Container>
             <ImgSlider/>
